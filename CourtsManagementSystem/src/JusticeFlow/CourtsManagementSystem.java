a841b1242a3950929603cfd29626ec2792fd2973
Saffi Muhammad Hashir <saffimhashir123@gmail.com>
2024-11-12 17:32:25 +0500

    Update CourtsManagementSystem.java

--- CourtsManagementSystem/src/JusticeFlow/CourtsManagementSystem.java
+++ CourtsManagementSystem/src/JusticeFlow/CourtsManagementSystem.java
@@ -7,29 +7,29 @@ import java.util.Scanner;
 public class CourtsManagementSystem {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
         DatabaseHandler dbHandler = new DatabaseHandler();
+dbHandler.Register();
+        // System.out.println("Welcome to the Courts Management System!");
 
-        System.out.println("Welcome to the Courts Management System!");
+        // Case hardcodedCase = new Case(
+        //         19,
+        //         "Hardcoded Case Title",
+        //         "Civil",
+        //         "Pending",
+        //         new Date(),
+        //         new Date(),
+        //         1,
+        //         2,
+        //         new ArrayList<>());
 
-        Case hardcodedCase = new Case(
-                19,
-                "Hardcoded Case Title",
-                "Civil",
-                "Pending",
-                new Date(),
-                new Date(),
-                1,
-                2,
-                new ArrayList<>());
+        // dbHandler.saveOrUpdateCase(hardcodedCase);
+        // hardcodedCase.updateCaseFiles();
 
-        dbHandler.saveOrUpdateCase(hardcodedCase);
-        hardcodedCase.updateCaseFiles();
+        // System.out.println("Hardcoded case added to the database:");
+        // System.out.println(hardcodedCase);
 
-        System.out.println("Hardcoded case added to the database:");
-        System.out.println(hardcodedCase);
-
-        System.out.println("Thank you for using the Courts Management System.");
-        scanner.close();
+        // System.out.println("Thank you for using the Courts Management System.");
+        // scanner.close();
     }
 }
